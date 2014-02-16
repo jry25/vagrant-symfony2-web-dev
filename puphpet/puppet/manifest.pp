3408177ad8c502130858e2c812dadf71a3efc42e
jry25 <jeremy.richard_87@yahoo.fr>
2014-02-16 19:19:52 +0100

    - add public network to make the machine reachable from outside the host - add Symfony related configurations (cache and log folder) - use nfs shared folder to improve performance

--- puphpet/puppet/manifest.pp
+++ puphpet/puppet/manifest.pp
@@ -359,6 +359,43 @@ if count($apache_values['modules']) > 0 {
   apache_mod { $apache_values['modules']: }
 }
 
+## Begin Symfony manifest
+
+if $symfony_values == undef {
+  $symfony_values = $yaml_values['symfony']
+}
+
+group { 'symfonians':
+  ensure => present,
+  members  => ['apache', 'nginx', 'httpd', 'www-data', $::ssh_username],
+  require => [
+    User['apache'],
+    User['nginx'],
+    User['httpd'],
+    User['www-data'],
+    User[$::ssh_username]
+  ]
+}
+
+$symfony_cache_location = $symfony_values['cache_and_logs_dir']
+
+exec { "exec mkdir -p ${symfony_cache_location}":
+  command => "mkdir -p ${symfony_cache_location}",
+  creates => $symfony_cache_location,
+}
+
+if ! defined(File[$symfony_cache_location]) {
+  file { $symfony_cache_location:
+    ensure  => directory,
+    group   => 'symfonians',
+    mode    => 0777,
+    require => [
+      Exec["exec mkdir -p ${symfony_cache_location}"],
+      Group['symfonians']
+    ]
+  }
+}
+
 ## Begin PHP manifest
 
 if $php_values == undef {
